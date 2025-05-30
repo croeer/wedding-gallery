df711b1ddb7e7a585bf6cbb0da9c212d0e72e6f3
croeer <christian@roeer.info>
2025-05-30 22:05:10 +0200

    Add subtitle2 class to App.css and include a new paragraph in App.tsx for additional content.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,13 @@ function App() {
       <div className="subtitle text-center text-cadbury">
         Wir haben ja gesagt!
       </div>
+
+      <div className="paragraph text-center text-cadbury mb-4">
+        Ein Tag. So viele Lieblingsmomente. Danke an alle, die mit uns gefeiert,
+        an uns gedacht oder diesen Tag einfach besonders gemacht haben. Hier
+        gibt’s Bilder zum Schmunzeln, Staunen und Zurückträumen.
+      </div>
+
       <Gallery />
       <ScrollToTop />
     </Container>
